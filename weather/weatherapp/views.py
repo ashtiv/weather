c6c573fa08edff6f2d9f63e7ef4f0b3761d2eb4c
Ashish Tiwari <75435020+ashtiv@users.noreply.github.com>
2021-01-18 20:03:56 +0530

    Update views.py

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -53,3 +53,34 @@ def naaam(request,x):
             
         response["weather"][0]["icon"]="http://openweathermap.org/img/wn/"+response["weather"][0]["icon"]+"@2x.png"
         return render(request, 'name1.html', context ={"response":response})
+def bnaaam(request):
+    # response.objects.filter(name=pp)
+    # if response["weather"][0]["main"]=="Smoke":
+    #     response["weather"][0]["main"]=link.png
+    # else :
+    #     response["weather"][0]["main"]=link.png
+    # response = dumps(response)
+    url='https://api.openweathermap.org/data/2.5/weather?q='
+    city=' '
+    api='&appid=a563bee744d5041f8605a3ceffaac26d'
+    url=url+city+api
+    global response
+    response = requests.request("GET", url)
+    response=json.loads(response.text)
+    if len(response)<4:
+        response=' '
+        return render(request, 'name2.html',context={"response":response})
+
+    else :
+        response["main"]["temp"]=round(response["main"]["temp"]-273.15,2)
+        response["main"]["temp_max"]=round(response["main"]["temp_max"]-273.15,2)
+        response["main"]["temp_min"]=round(response["main"]["temp_min"]-273.15,2)
+        if "country" in response["sys"]:
+            response["sys"]["flag"]=flag.flag(response["sys"]["country"])
+            crl='https://restcountries.eu/rest/v2/alpha?codes='+response["sys"]["country"]
+            rr=requests.request("GET", crl)
+            rr=json.loads(rr.text)
+            response["sys"]["country"]=','+rr[0]["name"]
+            
+        response["weather"][0]["icon"]="http://openweathermap.org/img/wn/"+response["weather"][0]["icon"]+"@2x.png"
+        return render(request, 'name1.html', context ={"response":response})
